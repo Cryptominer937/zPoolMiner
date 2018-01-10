95d171217d26f369795b6447230c378d1ed2dc6a
Cryptominer937 <miners@grassbustersofdayton.com>
2018-01-10 14:58:33 -0500

    Bin Location

--- zPoolMiner/Utils/MinersDownloadManager.cs
+++ zPoolMiner/Utils/MinersDownloadManager.cs
@@ -15,12 +15,12 @@ using zPoolMiner.Devices;
 namespace zPoolMiner.Utils {
     public static class MinersDownloadManager {
         public static DownloadSetup StandardDlSetup = new DownloadSetup(
-            "https://github.com/Cryptominer937/zPoolMiner/raw/Miner-Additions/Bins/bin_1_8_1_6.zip",
+            "http://github.com/Cryptominer937/zPoolMiner/releases/download/1.8.1.7-BINS/bin-1.8.1.7.zip",
             "bins.zip",
             "bin");
 
         public static DownloadSetup ThirdPartyDlSetup = new DownloadSetup(
-            "https://github.com/Cryptominer937/zPoolMiner/raw/Miner-Additions/Bins/bin_3rdparty_1_8_1_6.zip",
+            "http://github.com/Cryptominer937/zPoolMiner/releases/download/1.8.1.7-BINS/bin_3rdparty-1.8.1.7.zip",
             "bins_3rdparty.zip",
             "bin_3rdparty");
     }
