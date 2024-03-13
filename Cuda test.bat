cabb0505e3078ac8c02db5dd28b52ab0d802af46
Hash-Kings <miners@grassbustersofdayton.com>
2024-03-13 18:41:40 -0400

    3.0.2.1

---
+++ Cuda test.bat
@@ -0,0 +1,2 @@
+CudaDeviceDetection
+pause
---
+++ check_all_AMD.bat
@@ -0,0 +1,14 @@
+"AMDOpenCLDeviceDetection.exe -p" > debug_all_out_AMD.txt
+AMDOpenCLDeviceDetection.exe -p >> debug_all_out_AMD.txt
+"bin\sgminer-5-6-0-general\sgminer --gpu-platform 0 --ndevs" >> debug_all_out_AMD.txt
+bin\sgminer-5-6-0-general\sgminer --gpu-platform 0 --ndevs >> debug_all_out_AMD.txt
+"bin\sgminer-5-6-0-general\sgminer --gpu-platform 1 --ndevs" >> debug_all_out_AMD.txt
+bin\sgminer-5-6-0-general\sgminer --gpu-platform 1 --ndevs >> debug_all_out_AMD.txt
+"bin\sgminer-5-6-0-general\sgminer --gpu-platform 2 --ndevs" >> debug_all_out_AMD.txt
+bin\sgminer-5-6-0-general\sgminer --gpu-platform 2 --ndevs >> debug_all_out_AMD.txt
+"bin\sgminer-gm\sgminer --gpu-platform 0 --ndevs" >> debug_all_out_AMD.txt
+bin\sgminer-gm\sgminer --gpu-platform 0 --ndevs >> debug_all_out_AMD.txt
+"bin\sgminer-gm\sgminer --gpu-platform 1 --ndevs" >> debug_all_out_AMD.txt
+bin\sgminer-gm\sgminer --gpu-platform 1 --ndevs >> debug_all_out_AMD.txt
+"bin\sgminer-gm\sgminer --gpu-platform 2 --ndevs" >> debug_all_out_AMD.txt
+bin\sgminer-gm\sgminer --gpu-platform 2 --ndevs >> debug_all_out_AMD.txt
