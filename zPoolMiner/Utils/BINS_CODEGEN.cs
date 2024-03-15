efed6b9fb95006d02e62921d97f8d1cc75189f79
Hash-Kings <miners@grassbustersofdayton.com>
2024-03-15 12:11:34 -0400

    Start Fixing File missing detection

--- zPoolMiner/Utils/BINS_CODEGEN.cs
+++ zPoolMiner/Utils/BINS_CODEGEN.cs
@@ -5,7 +5,7 @@ namespace zPoolMiner.Utils
         #region CODE_GEN STUFF // listFiles.py
 
         public static string[] ALL_FILES_BINS = {
-            /*@"\AMD\mkxminer\mkxminer.exe",
+            //@"\AMD\mkxminer\mkxminer.exe",
             //@"\AMD\sgminer-5-6-0-general\sgminer.exe",
             //@"\AMD\sgminer-gm\sgminer.exe",
             //@"\AMD\sgminer-HSR\sgminer.exe",
@@ -16,23 +16,22 @@ namespace zPoolMiner.Utils
             //@"\AMD\sgminer-Xevan\sgminer.exe",
             //@"\AMD\sgminer-aceneun\sgminer.exe",
             //@"\AMD\GLG\gatelessgate.exe",
-            @"\CPU\CPU-cpuminerOptBF\cpuminer-aes-sse42.exe",
-            @"\CPU\CPU-JayDDee\cpuminer-aes-sse42.exe",
-            @"\CPU\CPU-JayDDee\cpuminer-avx2.exe",
-            @"\CPU\CPU-Cpupower\cpuminer-aes-sse42.exe",
-            @"\CPU\CPU-JayDDee\cpuminer-aes-sse42.exe",
-            @"\CPU\CPU-JayDDeeYespower\cpuminer-sse42.exe",
-            @"\CPU\CPU-nosuch\cpuminer-aes-sse2.exe",
-            @"\CPU\CPU-Verium\cpuminer.exe",
-            @"\CPU\CPU-RKZ\cpuminer.exe",
-            @"\CPU\CPU-rplant\cpuminer-aes.exe",
-            @"\CPU\CPU-SRBMiner\SRBMiner-MULTI.exe",
+            //@"\CPU\CPU-cpuminerOptBF\cpuminer-aes-sse42.exe",
+            //@"\CPU\CPU-JayDDee\cpuminer-aes-sse42.exe",
+            //@"\CPU\CPU-JayDDee\cpuminer-avx2.exe",
+            //@"\CPU\CPU-Cpupower\cpuminer-sse2.exe",
+            //@"\CPU\CPU-JayDDeeYespower\cpuminer-sse42.exe",
+            //@"\CPU\CPU-nosuch\cpuminer-aes-sse2.exe",
+            //@"\CPU\CPU-Verium\cpuminer.exe",
+            //@"\CPU\CPU-RKZ\cpuminer.exe",
+            //@"\CPU\CPU-rplant\cpuminer-aes.exe",
+            //@"\CPU\CPU-SRBMiner\SRBMiner-MULTI.exe",
             @"\CPU\CPU-XMRig\xmrig.exe",
             @"\CPU\CPU-XMRigUPX\xmrig.exe",
             @"\NVIDIA\ccminer_tpruvot2\ccminer.exe",
             @"\NVIDIA\NVIDIA-trex\t-rex.exe",
             @"\NVIDIA\NVIDIA-CryptoDredge\CryptoDredge.exe",
-            @"\NVIDIA\NVIDIA-zealotenemy\z-enemy.exe",*/
+            @"\NVIDIA\NVIDIA-zealotenemy\z-enemy.exe",
             @"\vc_redist.x64.exe",
 };
 
