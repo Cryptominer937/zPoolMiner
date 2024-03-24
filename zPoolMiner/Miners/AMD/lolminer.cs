d62634f55e6b2d5db949c77526ce67dd9a95ad68
Hash-Kings <miners@grassbustersofdayton.com>
2024-03-24 18:27:26 -0400

    Update lolminer.cs

--- zPoolMiner/Miners/AMD/lolminer.cs
+++ zPoolMiner/Miners/AMD/lolminer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -157,7 +158,7 @@ namespace NiceHashMiner.Miners
             }
             string username = GetUsername(btcAddress, worker);
 
-            LastCommandLine = " --algo KARLSEN" + " --pool=" + url + " --user=" + username + ":" + worker + " --devices AMD --watchdog exit --apihost 127.0.0.1 --apiport " + ApiPort;
+            LastCommandLine = " --algo KARLSEN" + " --pool=" + url + " --user=" + username + " --pass " + worker + " --devices AMD --watchdog exit --apihost 127.0.0.1 --apiport " + ApiPort;
 
             LastCommandLine += GetDevicesCommandString();
 
@@ -177,9 +178,9 @@ namespace NiceHashMiner.Miners
             string username = Globals.GetBitcoinUser();
 
             if (ConfigManager.GeneralConfig.WorkerName.Length > 0)
-                username += "." + ConfigManager.GeneralConfig.WorkerName.Trim();
+                username += ":" + ConfigManager.GeneralConfig.WorkerName.Trim();
 
-            CommandLine = "--algo KARLSEN --pool" + url + "--user " + username + "--devices AMD --watchdog exit --apihost 127.0.0.1 --apiport " + ApiPort;
+            CommandLine = "--algo KARLSEN --pool " + url + " --user " + "DE8BDPdYu9LadwV4z4KamDqni43BUhGb66 --pass Benchmark " + "--devices AMD --watchdog exit --apihost 127.0.0.1 --apiport " + ApiPort + " --shortstats 120";
 
             CommandLine += GetDevicesCommandString(); //amd карты перечисляются первыми
 
@@ -190,17 +191,16 @@ namespace NiceHashMiner.Miners
         protected override bool BenchmarkParseLine(string outdata)
         {
             string hashSpeed = "";
-
             //Average speed (30s): 25.5 sol/s 
             //GPU 3: Share accepted (45 ms)
-            if (outdata.Contains("Average speed (15s):"))
+            if (outdata.Contains("Average speed (120s):"))
             {
-                int i = outdata.IndexOf("Average speed (15s):");
+                int i = outdata.IndexOf("Average speed (120s):");
                 int k = outdata.IndexOf("Mh/s");
                 hashSpeed = outdata.Substring(i + 21, k - i - 22).Trim();
                 try
                 {
-                    speed = speed + Double.Parse(hashSpeed, CultureInfo.InvariantCulture);
+                    speed = speed *1000000 + Double.Parse(hashSpeed, CultureInfo.InvariantCulture);
                 }
                 catch
                 {
@@ -212,12 +212,12 @@ namespace NiceHashMiner.Miners
                 count++;
             }
 
-            /*if (outdata.Contains("Share accepted") && speed != 0)
+            if (outdata.Contains("Share accepted") && speed != 0)
             {
-                BenchmarkAlgorithm.BenchmarkSpeed = speed / count;
+                BenchmarkAlgorithm.BenchmarkSpeed = speed / count *1000000;
                 BenchmarkSignalFinnished = true;
                 return true;
-            }*/
+            }
 
             return false;
 
@@ -244,28 +244,26 @@ namespace NiceHashMiner.Miners
         // TODO _currentMinerReadStatus
         public override async Task<APIData> GetSummaryAsync()
         {
-            Helpers.ConsolePrint("try API...........", "");
             var ad = new APIData(MiningSetup.CurrentAlgorithmType);
             string ResponseFromlolMiner;
-            double total = 0;
             try
             {
                 HttpWebRequest WR = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:" + ApiPort.ToString() + "/summary");
                 WR.UserAgent = "GET / HTTP/1.1\r\n\r\n";
-                WR.Timeout = 30 * 1000;
+                WR.Timeout = 3 * 1000;
                 WR.Credentials = CredentialCache.DefaultCredentials;
                 WebResponse Response = WR.GetResponse();
                 Stream SS = Response.GetResponseStream();
-                SS.ReadTimeout = 20 * 1000;
+                SS.ReadTimeout = 2 * 1000;
                 StreamReader Reader = new StreamReader(SS);
                 ResponseFromlolMiner = await Reader.ReadToEndAsync();
-                Helpers.ConsolePrint("API...........", ResponseFromlolMiner);
+                //Helpers.ConsolePrint("API: ", ResponseFromlolMiner);
                 //if (ResponseFromlolMiner.Length == 0 || (ResponseFromlolMiner[0] != '{' && ResponseFromlolMiner[0] != '['))
                 //    throw new Exception("Not JSON!");
                 Reader.Close();
                 Response.Close();
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return null;
             }
@@ -275,19 +273,49 @@ namespace NiceHashMiner.Miners
                 CurrentMinerReadStatus = MinerApiReadStatus.NONE;
                 return null;
             }
-            dynamic resp = JsonConvert.DeserializeObject(ResponseFromlolMiner);
-            if (resp != null)
+            try
             {
-                double totals = resp.Session.Performance_Summary;
-                ad.Speed = totals;
-                if (ad.Speed == 0)
+                dynamic resp = JsonConvert.DeserializeObject(ResponseFromlolMiner);
+                int mult = 1;
+                if (resp != null)
                 {
-                    CurrentMinerReadStatus = MinerApiReadStatus.READ_SPEED_ZERO;
+                    int gpus = resp.Session.Active_GPUs;
+                    double totals = resp.Session.Performance_Summary;
+                    if (MiningSetup.CurrentAlgorithmType == AlgorithmType.karlsenhash)
+                    {
+                        mult = 1000000;
+                    }
+                    else
+                    {
+                        mult = 1;
+                    }
+                    ad.Speed = totals * mult;
+                    if (gpus > 0)
+                    {
+                        double[] hashrates = new double[gpus];
+                        for (var i = 0; i < gpus; i++)
+                        {
+                            hashrates[i] = resp.GPUs[i].Performance;
+                        }
+                        int dev = 0;
+                        var sortedMinerPairs = MiningSetup.MiningPairs.OrderBy(pair => pair.Device.BusID).ToList();
+                        
+
+
+                        if (ad.Speed == 0)
+                        {
+                            CurrentMinerReadStatus = MinerApiReadStatus.READ_SPEED_ZERO;
+                        }
+                        else
+                        {
+                            CurrentMinerReadStatus = MinerApiReadStatus.GOT_READ;
+                        }
+                    }
                 }
-                else
-                {
-                    CurrentMinerReadStatus = MinerApiReadStatus.GOT_READ;
-                }
+            }
+            catch (Exception e)
+            {
+                Helpers.ConsolePrint(MinerTag(), e.ToString());
             }
 
             Thread.Sleep(100);
